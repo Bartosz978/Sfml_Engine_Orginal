20608ebabda92311dc9e96137d134d13abc39fe5
Bartosz Kaczmarczyk <bartosz.kaczmarczyk.pl@gmail.com>
2023-11-01 21:40:51 +0100

    Mala aktualizacja

--- EngineSFML/Engine.cpp
+++ EngineSFML/Engine.cpp
@@ -1,1 +1,12 @@
 #include "Engine.h"
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+int Engine::inicjalizajcja(unsigned int X, unsigned int Y,string Tytul){
+
+	window.create(VideoMode(X, Y), Tytul);
+
+	return 0;
+}
--- EngineSFML/Engine.h
+++ EngineSFML/Engine.h
@@ -1,5 +1,12 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
 class Engine
 {
+public:
+	sf::RenderWindow window;
+
+	int inicjalizajcja(unsigned int X,unsigned int Y, std::string Tytul);
+		
 };
 
