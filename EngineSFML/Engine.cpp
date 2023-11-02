389d7712d7b4409cd3b9cb98f82dfc8f6da8d6fc
Bartosz Kaczmarczyk <bartosz.kaczmarczyk.pl@gmail.com>
2023-11-02 20:43:30 +0100

    Dodaj

--- EngineSFML/Engine.cpp
+++ EngineSFML/Engine.cpp
@@ -29,4 +29,39 @@ void Engine::Klawiatura(char przycisk, void(*wskaznik_na_funckje)(),Event event)
 		
 	}
 }
+/*
+void Engine::wypelnij_ekran(Color kolor) {
+	window.clear(kolor);
+	window.display();
+}*/
+void Engine::Mysz_prawy_nacisniety(void(*wskaznik_na_funckje)(), Event event) {
+	if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Right) {
+		wskaznik_na_funckje();
+	}
+}
+
+void Engine::Mysz_prawy_zrelaksowany(void(*wskaznik_na_funckje)(), Event event) {
+	if (event.type == Event::MouseButtonReleased && event.mouseButton.button == Mouse::Right) {
+		wskaznik_na_funckje();
+	}
+}
+
+void Engine::Mysz_lewy_nacisniety(void(*wskaznik_na_funckje)(), Event event) {
+	if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
+		wskaznik_na_funckje();
+	}
+}
+
+void Engine::Mysz_lewy_zrelaksowany(void(*wskaznik_na_funckje)(), Event event) {
+	if (event.type == Event::MouseButtonReleased && event.mouseButton.button == Mouse::Left) {
+		wskaznik_na_funckje();
+	}
+}
+
+void Engine::Mysz_Ruch_nacisniety(void(*wskaznik_na_funckje)(), Event event) {
+	if (event.type == Event::MouseMoved && event.mouseButton.button == Mouse::Left) {
+		wskaznik_na_funckje();
+	}
+}
+
 void Engine::run(){}
--- EngineSFML/EngineSFML.cpp
+++ EngineSFML/EngineSFML.cpp
@@ -1,10 +1,13 @@
 ﻿#include <SFML/Graphics.hpp>
 #include "Engine.h"
 #include "PrimitiveRenderer.h"
+#include <iostream>
 
 using namespace std;
 using namespace sf;
-
+void f1() {
+    cout << "Dzialam" << endl;
+}
 
 
 int main()
@@ -12,15 +15,24 @@ int main()
    Engine gra;
     gra.inicjalizajcja(400, 400, "Gra", false);
     PrimitiveRenderer GRA;
-    
+    //gra.wypelnij_ekran(Color::Magenta);
 GRA.put_pixel(gra.window,100, 100, Color::Red);
 //GRA.rysuj_linie(gra.window,1,100,400,400,Color::Blue);
 GRA.rysuj_kwadrat(gra.window, 10, 100, 100, 10, 10, 10, 100, 100, Color::Blue);
 GRA.rysuj_trojkat(gra.window, 10, 50, 60, 40, 40, 90,Color::Red);
-  
+void(*wskaznik)() = &f1;
+while (true)
+{
+    sEvent event;
+    if (gra.window.pollEvent(event)) {
+        gra.Mysz_prawy_nacisniety(wskaznik, event);
+    }
+}
+
+
+
     gra.petla_gry();
    
-  //  Przyklad F1;
-    //F1.inicjalizajcja(600, 700, "tEST,", false);
+  
     return 0;
 }
